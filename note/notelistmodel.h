440b75e5632753de94d402129ce8587933e1abf9
Kanzey <kamil1pl@gmail.com>
2018-03-13 15:31:51 +0100

    Created NoteListModel

---
+++ note/notelistmodel.h
@@ -0,0 +1,16 @@
+#ifndef NOTELISTMODEL_H
+#define NOTELISTMODEL_H
+
+
+class NoteListModel : public QObject
+{
+    Q_OBJECT
+public:
+    explicit NoteListModel(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // NOTELISTMODEL_H
